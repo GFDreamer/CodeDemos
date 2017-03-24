12da21aed20e476d2aa45ef6b681a40c72a75831
QDD.lv <1679606091@qq.com>
2017-03-24 10:03:00 +0800

    创建了一个动画的文件夹

---
+++ CodeDemos/CodeDemos/demos/动画解析/AnimalExamplesViewController.h
@@ -0,0 +1,13 @@
+//
+//  AnimalExamplesViewController.h
+//  CodeDemos
+//
+//  Created by qddios2 on 17/3/24.
+//  Copyright © 2017年 lvguifeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AnimalExamplesViewController : UIViewController
+
+@end
---
+++ CodeDemos/CodeDemos/demos/动画解析/AnimalExamplesViewController.m
@@ -0,0 +1,37 @@
+//
+//  AnimalExamplesViewController.m
+//  CodeDemos
+//
+//  Created by qddios2 on 17/3/24.
+//  Copyright © 2017年 lvguifeng. All rights reserved.
+//
+
+#import "AnimalExamplesViewController.h"
+
+@interface AnimalExamplesViewController ()
+
+@end
+
+@implementation AnimalExamplesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
